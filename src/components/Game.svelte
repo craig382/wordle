0888d9ac2df2f6bd576eceab6df437ad9dfd45cb
craig382 <jazcfam@gmail.com>
2024-02-11 10:44:04 -0500

    Created Game.svelte\processValidGuess(). (Moved some code from submitWord() to its own method.)

--- src/components/Game.svelte
+++ src/components/Game.svelte
@@ -57,6 +57,23 @@
 	let board: Board;
 	let timer: Timer;
 
+	/**
+	 * Processes a valid guess, including:
+	 * updating the colors of the guess tiles,
+	 * updating  the colors of the keyboard tiles,
+	 * and updating the GameState
+	 * (including bot calculations).
+	 */
+	 export function processValidGuess() {
+			game.board.state[game.nGuesses] = game.guess(solution);
+			++game.nGuesses;
+			$letterStates.update(game.lastState, game.lastWord);
+			$letterStates = $letterStates;
+			game.update();
+			if (game.lastWord === solution) win();
+			else if (game.nGuesses === ROWS) lose();
+	}
+
 	function submitWord() {
 		if (game.latestWord.length !== COLS) {
 			toaster.pop("Not enough letters");
@@ -80,14 +97,7 @@
 					game.validHard = false;
 				}
 			}
-			// Submitted word is a valid guess. Process it.
-			game.board.state[game.nGuesses] = game.guess(solution);
-			++game.nGuesses;
-			$letterStates.update(game.lastState, game.lastWord);
-			$letterStates = $letterStates;
-			game.update();
-			if (game.lastWord === solution) win();
-			else if (game.nGuesses === ROWS) lose();
+			processValidGuess();
 		} else {
 			toaster.pop("Not in word list");
 			board.shake(game.nGuesses);
